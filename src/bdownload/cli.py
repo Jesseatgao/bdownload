c5edfd6c46c1f990da5b3021fa64c2e103e69cc5
cx <changxigao@gmail.com>
2020-10-09 16:46:48 +0800

    Add size unit suffixes(i.e. 'K' 'M') support for command line options parser

--- src/bdownload/cli.py
+++ src/bdownload/cli.py
@@ -1,14 +1,28 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentTypeError
 from os.path import join, normpath, abspath
+import re
 
 from .download import BDownloader
 
 DEFAULT_MAX_WORKER = 20
 DEFAULT_MIN_SPLIT_SIZE = 1024*1024
 DEFAULT_CHUNK_SIZE = 1024*10
+
+
+def normalize_bytes_num(bytes_num):
+    if not re.match('^[1-9][0-9]*[kKmM]?$', bytes_num):
+        msg = '{!r} is invalid, use, for example, 1024, 10K, or 2M instead'.format(bytes_num)
+        raise ArgumentTypeError(msg)
+
+    try:
+        size = int(bytes_num)
+    except ValueError:
+        size = int(bytes_num[:-1])*1024*1024 if bytes_num[-1] in 'mM' else int(bytes_num[:-1])*1024
+
+    return size
 
 
 def arg_parser():
@@ -20,9 +34,12 @@ def arg_parser():
     parser.add_argument('-D', '--dir', default='.', dest='dir', help='path to save the downloaded files')
     parser.add_argument('-p', '--proxy', dest='proxy', default=None,
                         help='proxy in the form of "http://[user:pass@]host:port" or "socks5://[user:pass@]host:port" ')
-    parser.add_argument('-n', '--max-workers', dest='max_workers', default=DEFAULT_MAX_WORKER, type=int, help='number of worker threads')
-    parser.add_argument('-k', '--min-split-size', dest='min_split_size', default=DEFAULT_MIN_SPLIT_SIZE, type=int, help='file split size')
-    parser.add_argument('-s', '--chunk-size', dest='chunk_size', default=DEFAULT_CHUNK_SIZE, type=int, help='every request range size')
+    parser.add_argument('-n', '--max-workers', dest='max_workers', default=DEFAULT_MAX_WORKER, type=int,
+                        help='number of worker threads')
+    parser.add_argument('-k', '--min-split-size', dest='min_split_size', default=DEFAULT_MIN_SPLIT_SIZE,
+                        type=normalize_bytes_num, help='file split size, "1048576, 1024K or 2M" for example')
+    parser.add_argument('-s', '--chunk-size', dest='chunk_size', default=DEFAULT_CHUNK_SIZE, type=normalize_bytes_num,
+                        help='every request range size, "10240, 10K or 1M" for example')
     parser.add_argument('-e', '--cookie', dest='cookie', default=None,
                         help='cookies in the form of "cookie_key=cookie_value cookie_key2=cookie_value2"')
     parser.add_argument('--user-agent', dest='user_agent', default=None, help='custom user agent')
